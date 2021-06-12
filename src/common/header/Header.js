7690d8092bdff77852df0c33d0197c9fc37fc36a
ifra91 <ifra.khan.1991@gmail.com>
2021-06-12 15:11:36 +0530

    Checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import React from 'react';
+import './Header.css';
+
+
+
+class Header extends Component {
+    render() {
+      return ( 
+      <div className = "header">
+          
+      </div>
+      );
+    }
+  }
+
+  export default Header;
