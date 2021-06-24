9c00602b4e30a509669b668dc0c76317cd65d1a7
ifra91 <ifra.khan.1991@gmail.com>
2021-06-24 12:32:16 +0530

    Checkpoint 5

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from "./common/header/Header";
 import Home from "./screens/home/Home";
+import Details from "./screens/details/Details";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import moviesData from "./common/moviesData";
 
 ReactDOM.render(
   <div>
      {/* <Header /> */}
-    <Home />
+     <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route
+          path="/details/:id"
+          component={({ match }) => (
+            <Details match={match} movies={moviesData} />
+          )}
+        />
+      </Switch>
+    </Router>
   </div>,
   document.getElementById("root")
 );
